3fdcf9a8d662478933e163918a06722a13eae095
Matan <ml.git201@gmail.com>
2025-04-04 17:09:28 +0300

    feat(stream): added the stream provider for the root layout

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+import StreamClientProvider from "@/components/providers/StreamClientProvider";
+import React from "react";
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return <StreamClientProvider>{children}</StreamClientProvider>;
+}
+
+export default Layout;
