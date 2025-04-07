c3af2565442392ea390c71fde5f6e394636b903c
Matan <ml.git201@gmail.com>
2025-04-07 22:25:29 +0300

    feat(dashboard): added the dashboard page

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,7 @@ import { clsx, type ClassValue } from "clsx";
 import {
   addHours,
   intervalToDuration,
+  isAfter,
   isBefore,
   isWithinInterval,
 } from "date-fns";
@@ -9,6 +10,7 @@ import { twMerge } from "tailwind-merge";
 import { Doc } from "../../convex/_generated/dataModel";
 
 type Interview = Doc<"interviews">;
+type User = Doc<"users">;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -51,4 +53,51 @@ export const getMeetingStatus = (interview: Interview) => {
     return "live";
   if (isBefore(now, interviewStartTime)) return "upcoming";
   return "completed";
+};
+
+export const groupInterviews = (interviews: Interview[]) => {
+  if (!interviews) return {};
+
+  return interviews.reduce((acc: any, interview: Interview) => {
+    const date = new Date(interview.startTime);
+    const now = new Date();
+
+    if (interview.status === "succeeded") {
+      acc.succeeded = [...(acc.succeeded || []), interview];
+    } else if (interview.status === "failed") {
+      acc.failed = [...(acc.failed || []), interview];
+    } else if (isBefore(date, now)) {
+      acc.completed = [...(acc.completed || []), interview];
+    } else if (isAfter(date, now)) {
+      acc.upcoming = [...(acc.upcoming || []), interview];
+    }
+
+    return acc;
+  }, {});
+};
+
+export const getCandidateInfo = (users: User[], candidateId: string) => {
+  const candidate = users?.find((user) => user.clerkId === candidateId);
+  return {
+    name: candidate?.name || "Unknown Candidate",
+    image: candidate?.image || "",
+    initials:
+      candidate?.name
+        ?.split(" ")
+        .map((n) => n[0])
+        .join("") || "UC",
+  };
+};
+
+export const getInterviewerInfo = (users: User[], interviewerId: string) => {
+  const interviewer = users?.find((user) => user.clerkId === interviewerId);
+  return {
+    name: interviewer?.name || "Unknown Interviewer",
+    image: interviewer?.image,
+    initials:
+      interviewer?.name
+        ?.split(" ")
+        .map((n) => n[0])
+        .join("") || "UI",
+  };
 };
